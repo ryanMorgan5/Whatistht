e70f0a606bece5a0cc769bbb4f663f3a05c8d8c6
ryanMorgan5 <51963597+ryanMorgan5@users.noreply.github.com>
2019-06-18 12:37:47 -0400

    Add files via upload

---
+++ Calculator.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class Calculator
+{
+    public static void main(String[] args)
+    {
+        //Variables to be defined by scanner.
+        Scanner myScanner = new Scanner(System.in);
+        double firstOperand;
+        double secondOperand;
+        int operation;
+        double result;
+
+        System.out.println("Enter first operand:");
+        firstOperand = myScanner.nextDouble();
+
+        System.out.println("Enter second operand:");
+        secondOperand = myScanner.nextDouble();
+
+        System.out.println("Calculator Menu" +"\n---------------" +"\n1. Addition"+"\n2. Subtraction"+"\n3. Multiplication"+"\n4. Division");
+        System.out.println("Which operation do you want to perform?");
+        operation = myScanner.nextInt();
+
+        //operations to be determined (+-*/)
+        if (operation < 1 || operation > 4)
+        {
+            System.out.println("Error: Invalid selection! Terminating program.");
+        }
+        //If the operation variable falls within range then the operation will run.
+        else{
+            if (operation == 1)
+            {
+                result = firstOperand + secondOperand;
+            } else if (operation == 2)
+            {
+                result = firstOperand - secondOperand;
+            } else if (operation == 3)
+            {
+                result = firstOperand * secondOperand;
+            } else
+            {
+                result = firstOperand / secondOperand;
+            }
+
+            System.out.println("The result of the operation is " + result + ". Goodbye!");
+        }
+    }
+}
